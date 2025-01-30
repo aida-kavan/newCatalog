f5758fcb04bab46df9a0043ddbe5c99289015715
aida-kavan <aidahajikavan@gmail.com>
2025-01-30 19:53:29 +0330

    modified: vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
+import { swcReact } from 'vite-plugin-swc';
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: '/newCatalog/'
-})
+  plugins: [swcReact()],
+  build: {
+    outDir: 'dist',
+  },
+  base: "/newCatalog"
+});
